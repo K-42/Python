31d6e41c78331dd4857b7087a114e05fb63228b6
K-42 <k_miller42@hotmail.com>
2016-06-14 19:23:52 +0100

    Update MDScrape.py
    Improved interface.
    Added user-definable interval for loop (default: 5 minutes).

--- MDScrape.py
+++ MDScrape.py
@@ -1,4 +1,6 @@
-import requests, bs4
+#! python3
+
+import requests, bs4, datetime, time
 
 print ("  _  __  _  _     ____  ")
 print (" | |/ / | || |   |___ \ ")
@@ -10,15 +12,27 @@ print ("")
 url = requests.get('https://www.kickstarter.com/projects/coolminiornot/massive-darkness')
 soup = bs4.BeautifulSoup(url.text, "html.parser")
 cheese = soup.select('span.pledge__backer-count')
-  
-if cheese[0].getText() == '1,820 backers':
-    print("Early backers for Massive Darkness still at 1820. I'll text you if it drops.")
-else:
-    print("Early pledges for Massive Darkness are below maximum - I've sent a text: act fast!")
-    from twilio.rest import TwilioRestClient
-    accountSID = ''
-    authToken = ''
-    twilioCli = TwilioRestClient(accountSID, authToken)
-    myTwilioNumber = ''
-    targetNumber = ''
-    message = twilioCli.messages.create(body='An early backer has pulled out of Massive Darkness!', from_=myTwilioNumber, to=targetNumber)
+interval = 300
+print('Hello! This script checks whether any early backers have pulled out of the Massive Darkness kickstarter, potentially saving Ben the princely sum of 10 dollars.', end=' \n\n')
+print("It'll run once every " + str(interval) + " seconds. You can set a new interval value below or press enter to accept the default:")
+
+x = input()
+if x != '':
+    interval = x
+print('OK!', end=' \n\n')   
+
+print('Checking every ' +str(interval) + ' seconds. Current status:')
+while True:
+    if cheese[0].getText() == '1,820 backers':
+        print("I checked Massive Darkness at " + (str(datetime.datetime.now().time()))[:5]+ ". The slots for early backers were full.")
+        time.sleep(int(interval))
+    else:
+        print("An early backer slot for Massive Darkness is available so I've sent you a text - act fast!")
+        from twilio.rest import TwilioRestClient
+        accountSID = ''
+        authToken = ''
+        twilioCli = TwilioRestClient(accountSID, authToken)
+        myTwilioNumber = ''
+        targetNumber = ''
+        message = twilioCli.messages.create(body='Ben: an early backer has pulled out of Massive Darkness!', from_=myTwilioNumber, to=targetNumber)
+        break
